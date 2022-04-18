3b587d454be22f8718c65dca82a3812a7da39e1e
Alexander Peñaloza <alexanderpenaloza3@gmail.com>
2022-04-18 14:08:52 -0500

    Deploy v1.0.2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
   const SWIPPER = d.querySelector(".swiper-wrapper");
   const SLIDERS = d.querySelectorAll(".swiper-slide");
   const NUMBERS = d.querySelectorAll(".paso-number-content");
+  const PAGE2 = d.querySelector(".page-2");
   let current = 0;
 
   const debounce = (fn) => {
@@ -22,8 +23,9 @@
     before = current;
     index = e.deltaY < 0 ? current - 1 : current + 1;
     current =
-      index < 0 ? SLIDERS.length - 1 : index > SLIDERS.length - 1 ? 0 : index;
-    removeClassActive(before);
+      index < 0 ? SLIDERS.length - 1 : index > SLIDERS.length - 1 ? 2 : index;
+    if (index > SLIDERS.length - 1) updateView();
+    else removeClassActive(before);
   };
 
   const activateDirect = (e, i) => {
@@ -37,6 +39,25 @@
     SLIDERS[before].classList.remove("active");
     SLIDERS[current].classList.add("active");
   };
+
+  const updateView = (show = true) => {
+    if (show) {
+      PAGE2.classList.remove("hide");
+      PAGE2.classList.add("show");
+      setTimeout(
+        () =>
+          PAGE2.scrollIntoView({
+            behavior: "smooth",
+          }),
+        200
+      );
+    } else {
+      PAGE2.classList.remove("show");
+      PAGE2.classList.add("hide");
+    }
+  };
+
+  window.onscroll = () => window.scrollY === 0 && updateView(false);
 
   SWIPPER.addEventListener("wheel", debounce(scrollSection));
 
